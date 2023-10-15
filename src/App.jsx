405a0de4c257577ce442b40f4e78b4f9d8ee7cae
elvisowusu <owusuelvisgyasi@gmail.com>
2023-10-15 14:37:52 +0000

    added other form input fields

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,26 @@
-import React,{ useState } from 'react'
-import arrowImg from './assets/icon-arrow.svg'
-import './App.css'
+import React, { useState } from "react";
+import arrowImg from "./assets/icon-arrow.svg";
+import "./App.css";
 
 function App() {
-
   return (
-    <div className='font-Poppins bg-offWhite h-100vh'>
-     <div className='bg-white'>
-      <div>
-        <input 
-         type="text"
-        />
-        <input
-         type="text"
-        />
+    <div className="font-Poppins bg-offWhite h-100vh">
+      <div className="bg-white">
+        <div>
+          <form action="">
+            <label htmlFor="">DAY</label>
+            <input type="text" />
+            <label htmlFor="">MONTH</label>
+            <input type="text" />
+            <label htmlFor="">YEAR</label>
+            <input type="text" />
+          </form>
+        </div>
+        <div></div>
       </div>
-      <div>
-
-      </div>
-     </div>
-     {/* <img src={arrowImg} alt="" className='bg-Purple hover:bg-offBlack'/> */}
+      {/* <img src={arrowImg} alt="" className='bg-Purple hover:bg-offBlack'/> */}
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
