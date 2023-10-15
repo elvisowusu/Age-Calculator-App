c39e486f8b2e9dcd5d9ce0cee83d475342d2dcc2
elvisowusu <owusuelvisgyasi@gmail.com>
2023-10-15 03:14:47 +0000

    created customized colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,15 @@ export default {
   content: [],
   theme: {
     extend: {
-      
+      colors:{
+          Purple: `hsl(259, 100%, 65%)`,
+          lightRed: `hsl(0, 100%, 67%)`,
+          White: `hsl(0, 0%, 100%)`,
+          offWhite: `hsl(0, 0%, 94%)`,
+          lightGrey: `hsl(0, 0%, 86%)`,
+          smokeyGrey: `hsl(0, 1%, 44%)`,
+          offBlack: `hsl(0, 0%, 8%)`,
+      },
     },
   },
   plugins: [],
