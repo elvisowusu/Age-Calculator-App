95b7f5c033d9aa15fde43169595c5d5d2f18c8a4
elvisowusu <owusuelvisgyasi@gmail.com>
2023-10-15 14:29:03 +0000

    added the structure

--- src/App.jsx
+++ src/App.jsx
@@ -5,9 +5,16 @@ import './App.css'
 function App() {
 
   return (
-    <div className='font-Poppins '>
-     Begini
-     <img src={arrowImg} alt="" />
+    <div className='font-Poppins bg-offWhite h-100vh'>
+     <div className='bg-white'>
+      <div>
+        
+      </div>
+      <div>
+
+      </div>
+     </div>
+     {/* <img src={arrowImg} alt="" className='bg-Purple hover:bg-offBlack'/> */}
     </div>
   )
 }
