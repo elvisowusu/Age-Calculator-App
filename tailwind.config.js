032178d0a53e8321fbe3e03908a7df6254105008
elvisowusu <owusuelvisgyasi@gmail.com>
2023-10-16 13:01:07 +0000

    added more styles

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,7 +12,6 @@ export default {
       colors:{
           Purple: `hsl(259, 100%, 65%)`,
           lightRed: `hsl(0, 100%, 67%)`,
-          White: `hsl(0, 0%, 100%)`,
           offWhite: `hsl(0, 0%, 94%)`,
           lightGrey: `hsl(0, 0%, 86%)`,
           smokeyGrey: `hsl(0, 1%, 44%)`,
